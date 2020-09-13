c9552f91a12665d5313c73363ef750080a71cc75
Jay Barria <jaybarria@gmail.com>
2020-09-13 17:06:32 +0800

    Update libraries

---
+++ androidutilities/src/main/java/com/github/jairrab/androidutilities/eventobserver/EventObserver.kt
@@ -0,0 +1,12 @@
+package com.github.jairrab.androidutilities.eventobserver
+
+import androidx.lifecycle.Observer
+
+class EventObserver<T>(private val onEventUnhandledContent: (T) -> Unit) :
+    Observer<Event<T>> {
+    override fun onChanged(event: Event<T>?) {
+        event?.getContentIfNotHandled()?.let { value ->
+            onEventUnhandledContent(value)
+        }
+    }
+}
