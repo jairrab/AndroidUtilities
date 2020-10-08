8d8995517670807cfadfbe50a82ec1ddcfe0a320
Jay Barria <jaybarria@gmail.com>
2020-10-08 22:20:59 +0800

    update library

--- androidutilities/src/main/java/com/github/jairrab/androidutilities/extensionfunctions/PreferenceUtils.kt
+++ androidutilities/src/main/java/com/github/jairrab/androidutilities/extensionfunctions/PreferenceUtils.kt
@@ -2,10 +2,11 @@ package com.github.jairrab.androidutilities.extensionfunctions
 
 import android.content.SharedPreferences
 import androidx.core.content.edit
+import java.util.*
 
 fun SharedPreferences.saveDouble(
-    number: Double,
     key: String,
+    number: Double,
     commit: Boolean = false
 ) {
     val a = number.toLongBits()
@@ -19,6 +20,24 @@ fun SharedPreferences.getDouble(
     val a = this.getLong(key, -1)
     if (a == -1L) return defaultValue
     return a.toDoubleBits()
+}
+
+fun SharedPreferences.saveDate(
+    date: Date,
+    key: String,
+    commit: Boolean = false
+) {
+    val a = date.time
+    this.edit(commit = commit) { putLong(key, a) }
+}
+
+fun SharedPreferences.getDate(
+    key: String,
+    defaultValue: Date = Date()
+): Date {
+    val a = this.getLong(key, -1)
+    if (a == -1L) return defaultValue
+    return Date(a)
 }
 
 fun Double.toLongBits(): Long {
