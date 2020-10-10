a70b9cd02e497c407d812952914fb532d63d5b98
Jay Barria <jaybarria@gmail.com>
2020-10-10 09:24:17 +0800

    update library

--- androidutilities/src/main/java/com/github/jairrab/androidutilities/extensionfunctions/ToastUtils.kt
+++ androidutilities/src/main/java/com/github/jairrab/androidutilities/extensionfunctions/ToastUtils.kt
@@ -4,9 +4,9 @@ import android.content.Context
 import android.widget.Toast
 
 fun Context.showToast(text: String, duration: Int = Toast.LENGTH_LONG) {
-    Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+    Toast.makeText(this, text, duration).show()
 }
 
 fun Context.showToast(resId: Int, duration: Int = Toast.LENGTH_LONG) {
-    Toast.makeText(this, resId, Toast.LENGTH_LONG).show()
+    Toast.makeText(this, resId, duration).show()
 }
