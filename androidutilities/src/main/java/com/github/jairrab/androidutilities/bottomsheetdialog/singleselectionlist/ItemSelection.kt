74cd64960cdd28bab87f98e763bb3da9a6680409
jairrab <jaybarria@gmail.com>
2021-02-26 11:34:44 +0800

    Update ItemSelection.kt

--- androidutilities/src/main/java/com/github/jairrab/androidutilities/bottomsheetdialog/singleselectionlist/ItemSelection.kt
+++ androidutilities/src/main/java/com/github/jairrab/androidutilities/bottomsheetdialog/singleselectionlist/ItemSelection.kt
@@ -1,34 +1,14 @@
 package com.github.jairrab.androidutilities.bottomsheetdialog.singleselectionlist
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import java.io.Serializable
 
+@Parcelize
 data class ItemSelection(
     val name: String,
     val iconInfo: IconInfo? = null,
-) : Parcelable {
-    private constructor(parcel: Parcel) : this(
-        parcel.readString() ?: "",
-        parcel.readParcelable(IconInfo::class.java.classLoader)
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(name)
-        parcel.writeParcelable(iconInfo, flags)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<ItemSelection> {
-        override fun createFromParcel(parcel: Parcel): ItemSelection {
-            return ItemSelection(parcel)
-        }
-
-        override fun newArray(size: Int): Array<ItemSelection?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+    val dataSerializable: Serializable? = null,
+    val dataParcelable: Parcelable? = null,
+) : Parcelable
 
